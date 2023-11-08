f439f47f4eaf1bd3dddb68c3ed75ade802a12d36
MattVoros <matthiasvoros@gmail.com>
2023-11-08 10:28:48 +0100

    Création de la page pour ajouter un utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/data/appRouter.jsx
+++ src/data/appRouter.jsx
@@ -2,6 +2,7 @@ import App from "../App";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import CreationUtilisateur from "../pages/CreationUtilisateur";
 import Error from "../pages/Error";
 
 export default [
@@ -17,6 +18,10 @@ export default [
             {
                 path: "login",
                 element: <Login />
+            },
+            {
+                path: "creationUtilisateur",
+                element: <CreationUtilisateur />
             }
         ]
     }
