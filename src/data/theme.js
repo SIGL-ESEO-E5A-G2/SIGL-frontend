61a2b1428a0577acb438edd2931e7a164bdcb79d
arthurbourret <bourret.arthur@gmail.com>
2024-01-11 21:58:20 +0100

    application direction artistique

---
+++ src/data/theme.js
@@ -0,0 +1,9 @@
+export default {
+    fontFamily: 'League Spartan, Open Sans, sans-serif',
+    colors: {
+        blue: ["#A4BFE0", "#95B5DB", "#85ABD5", "#76A0D0", "#6796CB", "#588BC6", "#457DBF", "#3E76B6", "#396DA7"],
+        red: ["#FFD6D6", "#FFC2C2", "#FFADAD", "#FF9999", "#FF8585", "#FF7070", "#FF4E4E", "#FF4747", "#FF3333"]
+    },
+    // white: "#F4F8FF",
+    black: "#1E1E1E",
+}
